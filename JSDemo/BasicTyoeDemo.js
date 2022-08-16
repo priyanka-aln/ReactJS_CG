662d35bc98d103b2328848851fbfd7b35cf72993
Priyanka <priyanka@Priyankas-MacBook-Pro.local>
2022-08-16 21:26:16 +0530

    first JSDemo

---
+++ JSDemo/BasicTyoeDemo.js
@@ -0,0 +1,23 @@
+var firstName = "Priyanka";
+var age = 10;
+var salary = 10000.13;
+var gender = "F";
+var isEmployed = true;
+var doj = new Date(2021, 00, 02);
+
+console.log("type of firstName "  + typeof(firstName)); //string
+console.log("firstname "+firstName);
+
+console.log("type of age "  + typeof(age)); //number
+console.log("age "+age);
+
+console.log("type of salary " + typeof(salary)); //number
+console.log("salary "+salary);
+
+console.log("type of gender " + typeof(gender)); //string
+console.log("gender "+gender);
+
+console.log("type of isEmployed " + typeof(isEmployed)); //boolean
+console.log("isEmployed "+isEmployed);
+console.log("type of doj " + typeof(doj)); //object
+console.log("doj "+doj);
